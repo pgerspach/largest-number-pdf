c2a7c0df90c6a934d1d44d4b46b644e3f49fa253
Patrick Gerspach <pgerspach@dataminr.com>
2025-08-11 19:39:42 -0400

    returning number to client

--- play-scala-seed/app/controllers/HomeController.scala
+++ play-scala-seed/app/controllers/HomeController.scala
@@ -10,7 +10,7 @@ import services.PdfService
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents, pdfService: PdfService) extends BaseController {
+class HomeController @Inject()(val controllerComponents: ControllerComponents, pdfService: PdfService) extends BaseController with Logging {
 
   /**
    * Create an Action to render an HTML page.
@@ -20,7 +20,21 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents, p
    * a path of `/`.
    */
   def index() = Action { implicit request: Request[AnyContent] =>
-    pdfService.readPdf()
     Ok(views.html.index())
+  }
+
+  def largestNumber() = Action { implicit request: Request[AnyContent] =>
+    val largestNumber = pdfService.readPdf()
+    largestNumber.fold(
+      error => {
+        logger.error("Error reading PDF: " + error.getMessage)
+        InternalServerError("Failed to read PDF file.")
+      },
+      number => if (number.isEmpty) {
+        NotFound("No numbers found in the PDF.")
+      } else {
+        Ok(s"The largest number found in the PDF is: ${number.get}")
+      }
+    )
   }
 }
--- play-scala-seed/app/services/PdfService.scala
+++ play-scala-seed/app/services/PdfService.scala
@@ -10,35 +10,30 @@ import java.util.regex.Pattern
 import javax.inject.{Inject, Singleton}
 import scala.util.Try
 
-import scala.jdk.CollectionConverters._
-
 @Singleton
 class PdfService @Inject() (app: Application) extends Logging {
-  def readPdf() : Unit = {
+  def readPdf(): Try[Option[Long]] = {
     val tryDocument = Try {
       Loader.loadPDF(new RandomAccessReadBufferedFile(app.path + "/app/pdf_files/FY25_Air_Force_Working_Capital_Fund.pdf"))
     }
-    if (tryDocument.isFailure) {
-      logger.error("Failed to load PDF: " + tryDocument.failed.get.getMessage)
-      return
-    }
-    val document = tryDocument.get
-    val text = new PDFTextStripper().getText(document)
-    val scanner = new Scanner(text)
-    var largestNumber = Long.MinValue
-    while (scanner.hasNextLine) {
-      val numbersInLine = Pattern.compile("\\D+")
-        .split(scanner.nextLine())
-        .toList
-        .filter(s => !s.isBlank)
-        .map(_.toInt)
-      if(numbersInLine.nonEmpty) {
-        val largestNumberInLine = numbersInLine.max
-        if (largestNumberInLine > largestNumber) {
-          largestNumber = largestNumberInLine
+    tryDocument.map { document =>
+      val text = new PDFTextStripper().getText(document)
+      val scanner = new Scanner(text)
+      var largestNumber: Option[Long] = None
+      while (scanner.hasNextLine) {
+        val numbersInLine = Pattern.compile("\\D+")
+          .split(scanner.nextLine())
+          .toList
+          .filter(s => !s.isBlank)
+          .map(_.toInt)
+        if (numbersInLine.nonEmpty) {
+          val largestNumberInLine = numbersInLine.max
+          if (largestNumber.isEmpty || largestNumberInLine > largestNumber.get) {
+            largestNumber = Some(largestNumberInLine)
+          }
         }
       }
+      largestNumber
     }
-    logger.error(s"Largest number found in PDF: $largestNumber")
   }
 }
